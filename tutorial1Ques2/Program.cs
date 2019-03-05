ac6cc9c675baf7929eda32fb8846c312e1508019
Nowendran Nandakumara Munusamy <21602854@dut4life.ac.za>
2019-03-05 19:57:44 +0200

    Add project files.

---
+++ tutorial1Ques2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutorial1Ques2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            string Name = "";
+            string HomeTown = "";
+            string Hobby = "";
+            string PetHate = "";
+
+            //assign values
+            Name = "Kirstie Chetty";
+            HomeTown = "kwamashu";
+            Hobby = "Fighting with nowen ";
+            PetHate = "I dont know what this means";
+
+            //display 
+
+            Console.WriteLine("*************************************************");
+            Console.WriteLine("*" +"Name:"+ "        " + Name + "/" + "      *");
+            Console.WriteLine("*" +"HomeTown:"+ "        " + HomeTown + "/" + "       *");
+            Console.WriteLine("*" +"Hobby"+ "        " + Hobby + "/" + "        *");
+            Console.WriteLine("*" +"Pethate:"+ "        " + PetHate+ "/" + "       *");
+            Console.Read();
+        }
+    }
+}
